fb111185d1e11e264726f094b5ac0c2c96118a42
kkashchenko <kkashchenko@griddynamics.com>
2025-08-08 09:43:47 +0200

    Fix imports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import path from 'path';
+import * as path from 'node:path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
